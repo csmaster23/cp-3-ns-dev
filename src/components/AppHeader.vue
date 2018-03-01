a4214958c3c9b7a1747cc827d70d86bd13444e56
Tanner Norton <tanneranorton23@gmail.com>
2018-03-01 11:51:10 -0700

    web pack

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,96 @@
+<template>
+
+
+    <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#">Navbar w/ text</a>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+    </ul>
+        <span class="navbar-text">
+            Navbar text with an inline element
+        </span>
+    </div>
+    </nav>
+
+    <nav>
+    <label for="show-menu" class="show-menu">Show Menu</label>
+    <input type="checkbox" id="show-menu" role="button">
+        <ul id="menu">
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/profile">Profile</router-link></li>
+    </ul>   
+    </nav>
+    
+</template>
+
+<script>
+ export default {
+     name: 'AppHeader'
+ }
+</script>
+
+<style scoped>
+ /*Strip the ul of padding and list styling*/
+ .clear {
+     float: clear;
+ }
+ nav {
+     display: grid;
+ }
+ ul {
+     list-style-type:none;
+     margin:0;
+     padding:0;
+ }
+
+ /*Create a horizontal list with spacing*/
+ li {
+     display:inline-block;
+     float: left;
+     margin-right: 1px;
+ }
+ /*Style for menu links*/
+ li a {
+     display:block;
+     margin-right: 20px;
+     height: 50px;
+     text-align: center;
+     line-height: 50px;
+     color: #666;
+ }
+ /*Active color*/
+ li a.active {
+ }
+ /*Hover state for top level links*/
+ li:hover a {
+ }
+ /*Style 'show menu' label button and hide it by default*/
+ .show-menu {
+     text-decoration: none;
+     color: #fff;
+     background: #FF5035;
+     text-align: center;
+     padding: 10px 0;
+     display: none;
+ }
+ /*Hide checkbox*/
+ input[type=checkbox]{
+     display: none;
+ }
+ /*Show menu when invisible checkbox is checked*/
+ input[type=checkbox]:checked ~ #menu{
+     display: block;
+ }
+</style>
---
+++ src/components/Profile.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Customize Your Allstar Profile</h1>
+    <h2>{{ name }}</h2>
+    <img v-bind:src="imagePath"/>
+  </div>
+</template>
+
+<script>
+ export default {
+   name: 'Profile',
+   data () {
+     return {
+       name: 'Big Arm Brock',
+       imagePath: '/static/images/road-runner.jpg'
+     }
+   }
+ }
+</script>
+
+<style scoped>
+ img {
+     max-width: 500px;
+ }
+</style>
